4abf7f4be3be702919696874e2b8bedd4cc23489
Luke <luke343279@gmail.com>
2021-12-04 19:48:45 -0800

    project 3 done

--- src/project_3/mmap.cpp
+++ src/project_3/mmap.cpp
@@ -17,15 +17,14 @@ int main( int argc, char ** argv )
     cout << "Usage: " << argv[0] << " <inFile> <outFile>" << endl;
     return 1;
   }
-  // const char * inFile  = "inFile.txt";
-  // const char * outFile = "outFile.txt";
+
   const char * inFile  = argv[1];
   const char * outFile = argv[2];
 
-  int         pageSize = getpagesize();
   struct stat srcStat;
-  int         srcFD = open( inFile, O_RDONLY );
-  int         dstFD = open( outFile, O_RDWR | O_CREAT, 0700 );
+  int         pageSize = getpagesize();
+  int         srcFD    = open( inFile, O_RDONLY );
+  int         dstFD    = open( outFile, O_RDWR | O_CREAT | O_TRUNC, 0700 );
 
   if( srcFD < 0 )
   {
@@ -40,27 +39,26 @@ int main( int argc, char ** argv )
   if( dstFD < 0 )
   {
     cout << "Error creating output file: " << outFile << endl;
-    return 1;
+    exit( 1 );
   }
-  ftruncate( dstFD, srcStat.st_size );
+  if( ftruncate( dstFD, srcStat.st_size ) < 0 )
+  {
+    cout << "Error Resizing output file: " << outFile << endl;
+    exit( 1 );
+  }
+
   for( int i = 0; i < srcStat.st_size; i += pageSize )
   {
-    if( srcStat.st_size - i < pageSize )
-    {
-      pageSize = srcStat.st_size - i;
-      cout << "pageSize: " << pageSize << endl;
-    }
-    // cout << "Mapping " << i << " bytes" << endl;
     char * srcData = (char *) mmap( NULL, pageSize, PROT_READ, MAP_SHARED, srcFD, i );
     char * dstData = (char *) mmap( NULL, pageSize, PROT_WRITE, MAP_SHARED, dstFD, i );
-    if( srcData == nullptr or dstData == nullptr )
+    if( !srcData || !dstData )
     {
       cout << "Error mapping files" << endl;
       exit( 1 );
     }
     memcpy( dstData, srcData, pageSize );
-    munmap( srcData, srcStat.st_size );
-    munmap( dstData, srcStat.st_size );
+    munmap( srcData, pageSize );
+    munmap( dstData, pageSize );
   }
 
 
